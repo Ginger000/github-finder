3f5f3eeb250338dc99861d5d575cd325188ff57b
Ginger000 <linj2@illinois.edu>
2022-04-28 13:54:38 -0500

    enable navbar and setup routes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1 className='text-6xl' >Welcome</h1>
+        <p>This is a react practive project following the Udemy course 
+            React Front to End 2022
+        </p>
+    </div>
+  )
+}
+
+export default Home
